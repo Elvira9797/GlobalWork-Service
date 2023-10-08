13078a29d10523315d1c83edff61116834fa53c4
Elvira Lemak <elvralema@rambler.ru>
2023-10-08 12:24:59 +0200

    slider images and icons

--- src/js/vacancies-card-slider.js
+++ src/js/vacancies-card-slider.js
@@ -1,5 +1,19 @@
 import card from '../common/card-vacancies.json';
 
+const imageFiles = [
+  'chemical.jpg',
+  'chairs.jpg',
+  'jelly.jpg',
+  'insulation.jpg',
+  'forklift.jpg',
+  'energy-drinks.jpg',
+  'beer.jpg',
+  'ice-cream.jpg',
+  'pavement.jpg',
+];
+
+console.log(imageFiles);
+
 let swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   centeredSlides: false,
@@ -22,16 +36,22 @@ let swiper = new Swiper('.mySwiper', {
   },
 });
 
-card.job_listing.forEach(vacancy => {
+card.job_listing.forEach((vacancy, index) => {
   const vacancyCard = document.createElement('div');
   vacancyCard.classList.add('swiper-slide');
   vacancyCard.innerHTML = `
             <div class="vacancy-card">
-              <img class="vacancy-img" src="./images/chairs.jpg" alt="chairs" />
+            <div class="vacancy-img-wrap" style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4) 15%, rgba(252, 176, 69, 0) 50%), url('./images/${imageFiles[index]}');"> <h4 class="vacancy-title">${vacancy.job_title}</h4> </div>
               <div class="vacancy-data-wrap">
-                <p><b>City: </b>${vacancy.city}</p>
-                <p><b>Position: </b>${vacancy.position}</p>
-                <p><b>Salary: </b>${vacancy.salary}</p>
+                <p class="vacancy-data-item"><svg class="vacancy-icon" width="25" height="25">
+              <use href="./images/icons.svg#icon-location"></use>
+            </svg><b>City: </b>${vacancy.city}</p>
+                <p class="vacancy-data-item"><svg class="vacancy-icon" width="25" height="25">
+              <use href="./images/icons.svg#icon-user"></use>
+            </svg><b>Position: </b>${vacancy.position}</p>
+                <p class="vacancy-data-item"><svg class="vacancy-icon" width="25" height="25">
+              <use href="./images/icons.svg#icon-credit-card"></use>
+            </svg><b>Salary: </b>${vacancy.salary}</p>
               </div>
               <button type="button" class="vacancies-btn btn">More info</button>
             </div>
