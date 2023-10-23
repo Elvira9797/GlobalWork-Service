896f9a4caf9786e16bb445b0082b80a65e985c83
Elvira Lemak <elvralema@rambler.ru>
2023-10-23 18:13:04 +0200

    Fix

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,28 @@
 import Swiper from 'swiper/bundle';
-import { imageFiles } from './resources';
-import { data } from './resources';
+import card from '../common/card-vacancies.json';
+import fullInfoVacancies from '../common/full-info-vacancies.json';
+import chemical from '../images/chemical.jpg';
+import chairs from '../images/chairs.jpg';
+import jelly from '../images/jelly.jpg';
+import insulation from '../images/insulation.jpg';
+import forklift from '../images/forklift.jpg';
+import energy_drinks from '../images/energy-drinks.jpg';
+import beer from '../images/beer.jpg';
+import ice_cream from '../images/ice-cream.jpg';
+import pavement from '../images/pavement.jpg';
+import icons from '../images/icons.svg';
 
-const { fullInfoVacancies } = data;
-const { card, icons } = data;
+const imageFiles = [
+  chemical,
+  chairs,
+  jelly,
+  insulation,
+  forklift,
+  energy_drinks,
+  beer,
+  ice_cream,
+  pavement,
+];
 
 const swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
