855d6fd1c2ae373d1c4bffabe8f20befdb5351db
Elvira Lemak <elvralema@rambler.ru>
2025-01-13 14:21:37 +0100

    Add new countries

--- src/components/WorkPermits/WorkPermits.tsx
+++ src/components/WorkPermits/WorkPermits.tsx
@@ -7,6 +7,8 @@ import { ReactComponent as SerbiaSvg } from '../../assets/svg/Serbia.svg';
 import { ReactComponent as AlbaniaSvg } from '../../assets/svg/Albania.svg';
 import { ReactComponent as HungarySvg } from '../../assets/svg/Hungary.svg';
 import { ReactComponent as CroatiaSvg } from '../../assets/svg/Croatia.svg';
+import { ReactComponent as SloveniaSvg } from '../../assets/svg/Slovenia.svg';
+import { ReactComponent as MontenegroSvg } from '../../assets/svg/Montenegro.svg';
 import {
   DocsItemPrice,
   DocsItemTitle,
@@ -181,6 +183,51 @@ const WorkPermits: React.FC = () => {
               <p> {t('main.workPermitsArr.workPermit10.time')}</p>
               <DocsItemPrice>
                 {t('main.workPermitsArr.workPermit10.price')}
+              </DocsItemPrice>
+            </DocsItems>
+            <DocsItems>
+              <CroatiaSvg
+                style={{ margin: '0 auto', display: 'flex' }}
+                width={70}
+                height={70}
+              />
+              <DocsItemTitle>
+                {t('main.workPermitsArr.workPermit11.title')}
+              </DocsItemTitle>
+              <Bolt> {t('main.workPermitsArr.workPermit11.name')}</Bolt>
+              <p> {t('main.workPermitsArr.workPermit11.time')}</p>
+              <DocsItemPrice>
+                {t('main.workPermitsArr.workPermit11.price')}
+              </DocsItemPrice>
+            </DocsItems>
+            <DocsItems>
+              <SloveniaSvg
+                style={{ margin: '0 auto', display: 'flex' }}
+                width={70}
+                height={70}
+              />
+              <DocsItemTitle>
+                {t('main.workPermitsArr.workPermit12.title')}
+              </DocsItemTitle>
+              <Bolt> {t('main.workPermitsArr.workPermit12.name')}</Bolt>
+              <p> {t('main.workPermitsArr.workPermit12.time')}</p>
+              <DocsItemPrice>
+                {t('main.workPermitsArr.workPermit12.price')}
+              </DocsItemPrice>
+            </DocsItems>
+            <DocsItems>
+              <MontenegroSvg
+                style={{ margin: '0 auto', display: 'flex' }}
+                width={70}
+                height={70}
+              />
+              <DocsItemTitle>
+                {t('main.workPermitsArr.workPermit13.title')}
+              </DocsItemTitle>
+              <Bolt> {t('main.workPermitsArr.workPermit13.name')}</Bolt>
+              <p> {t('main.workPermitsArr.workPermit13.time')}</p>
+              <DocsItemPrice>
+                {t('main.workPermitsArr.workPermit13.price')}
               </DocsItemPrice>
             </DocsItems>
           </DocsList>
